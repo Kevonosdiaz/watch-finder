f5fb97e1e71f1849ae1f1a51b03243b618d53dea
Camila Hernandez <camila.hernandez@ucalgary.ca>
2026-03-27 17:10:34 -0600

    feat: make posters clickable buttons and add route to open a new page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,29 @@ import { useState } from "react";
 import Home from "./pages/Home";
 import Watchlist from "./pages/Watchlist";
 
-function App() {
-  const [page, setPage] = useState<"home" | "watchlist">("home");
+type Page = "home" | "watchlist" | "title";
 
+type SelectedWatchlistItem = {
+  watchlistId: number;
+  titleId: number;
+} | null;
+
+function App() {
+  const [page, setPage] = useState<Page>("home");
+  const [selectedTitle, setSeletectedTitle] = useState<SelectedWatchlistItem>(null);
+  const goToWatchdata = (watchlistId: number, titleId: number) => {
+    setSeletectedTitle({ watchlistId, titleId });
+    setPage("title");
+  }
   return (
     <>
       {page === "home" && <Home goToWatchlist={() => setPage("watchlist")} />}
-      {page === "watchlist" && <Watchlist goToHome={() => setPage("home")} />}
+      {page === "watchlist" && 
+        <Watchlist 
+          goToHome={() => setPage("home")}
+          goToWatchdata={goToWatchdata}
+        />
+      }
     </>
   );
 }
