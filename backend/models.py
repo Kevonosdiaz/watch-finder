cfc911192431a038c5676df0b22a2159b5b94a4e
Camila Hernandez <camila.hernandez@ucalgary.ca>
2026-04-02 10:29:54 -0600

    feat: set default timestamp for DateAdded field

--- backend/models.py
+++ backend/models.py
@@ -133,7 +133,7 @@ class Watchlists(Base):
                                                 String(255),
                                                 nullable=False)
     date_added: Mapped[date] = mapped_column(
-        "DateAdded", Date, server_default=func.current_date())
+        "DateAdded", Date, erver_default=Text("CURRENT_TIMESTAMP"))
 
 
 class WatchlistContains(Base):
