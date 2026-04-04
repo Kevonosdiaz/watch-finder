5cf4a2c2c86a483c53328abaa02de31cc1acb52d
Camila Hernandez <camila.hernandez@ucalgary.ca>
2026-04-03 18:36:52 -0600

    feat: add api call to handle login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { api } from "../api/client";
 import logo from "../assets/watch-finder-logo.png";
 
 type LoginProps = {
@@ -8,7 +9,19 @@ type LoginProps = {
 export default function Login({ onLogin }: LoginProps) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, ] = useState(false);
+    
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        try {
+            const loginUser = await api<any>(`/api/users/${email}`);
+            console.log("User: ", loginUser)
+            onLogin();
+        } catch (err) {
+        console.error("Login failed: ", err);
+        }  
+    }
     
     return (
         <div className="login-container">
@@ -41,7 +54,12 @@ export default function Login({ onLogin }: LoginProps) {
                         />
                     </div>
                 </div>
-                <button className="login-btn">Login</button>
+                <button 
+                    className="login-btn"
+                    onClick={handleLogin}
+                >
+                    Login
+                </button>
                 <div className="create-account-text">
                     Don't have an account? <a href="/signup">Sign up</a>
                 </div>
