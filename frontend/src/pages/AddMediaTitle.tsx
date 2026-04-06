8a694adca968f2cbfa5916d1ef76d700318d45e9
Camila Hernandez <camila.hernandez@ucalgary.ca>
2026-04-06 12:55:34 -0600

    feat: add most fields for creating a new media title

--- frontend/src/pages/AddMediaTitle.tsx
+++ frontend/src/pages/AddMediaTitle.tsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 
 interface AddMediaTitleProps {
   goBack: () => void;
 }
 
+type MediaTitle = {
+  title: string;
+  year: number | "";
+  kind?: "Movie" | "TV";
+  criticsScore?: number | "";
+  rating?: string;
+  runtime?: number | "";
+  number_of_seasons?: number | "";
+  creator?: string;
+  synopsis?: string;
+};
+
+
 export default function AddMediaTitle({ goBack }: AddMediaTitleProps){
-  return (
+  
+const [media, setMedia] = useState<MediaTitle>({
+    title: "",
+    year: "",
+    kind: undefined,
+    criticsScore: "",
+    rating: "",
+    runtime: "",
+    number_of_seasons: "",
+    creator: "",
+    synopsis: "",
+  });
+
+    return (
     <div className="admin-container">
       <div className="watchlist-header">
         <div className="back-btn">
@@ -19,7 +46,116 @@ export default function AddMediaTitle({ goBack }: AddMediaTitleProps){
         </div>
       </div>
       <div className="edit-form-box">
-        
+        <div className="edit-form-field">
+          <div className="form-label">Title</div>
+          <input
+            className="edit-form-field-input"
+            placeholder="Enter the title (e.g. Avatar: The Last Airbender)"
+            value={media.title}
+            onChange={(e) => setMedia({ ...media, title: e.target.value })}
+          />
+        </div>
+        <div className="edit-form-field">
+          <div className="form-label">Year</div>
+          <input
+            type="number"
+            className="edit-form-field-input"
+            placeholder="Enter the release year (e.g. 2005)"
+            value={media.year}
+            onChange={(e) =>
+              setMedia({ ...media, year: Number(e.target.value) })
+            }
+          />
+        </div>
+        <div className="edit-form-field">
+          <div className="form-label">Critics score</div>
+          <input
+            className="edit-form-field-input"
+            placeholder="Enter the critics score out of 10 (e.g. 9.3)"
+            value={media.criticsScore}
+            onChange={(e) =>
+              setMedia({ ...media, criticsScore: Number(e.target.value) })
+            }
+          />
+        </div>
+        <div className="edit-form-field">
+          <div className="form-label">Age rating</div>
+          <input
+            className="edit-form-field-input"
+            placeholder="Enter the age rating (e.g. TV-Y7-FV)"
+            value={media.rating}
+            onChange={(e) => setMedia({ ...media, rating: e.target.value })}
+          />
+        </div>
+        <div className="edit-form-field">
+          <div className="form-label">Movie or show?</div>
+          <select
+            className="edit-form-field-select"
+            value={media.kind}
+            onChange={(e) =>
+              setMedia({ ...media, kind: e.target.value as "Movie" | "TV" })
+            }
+          >
+            <option value="" disabled>
+                Select movie or TV show
+            </option>
+            <option value="Movie">Movie</option>
+            <option value="TV">TV Show</option>
+          </select>
+        </div>
+
+        {media.kind === "Movie" ? (
+          <div className="edit-form-field">
+            <div className="form-label">Movie runtime</div>
+
+<input
+              type="number"
+              className="edit-form-field-input"
+              placeholder="Enter the movie's runtime in minutes (e.g. 124)"
+              value={media.runtime}
+              onChange={(e) =>
+                setMedia({ ...media, runtime: Number(e.target.value) })
+              }
+            />
+          </div>
+        ) : (
+          <div className="edit-form-field">
+            <div className="form-label">Number of seasons</div>
+            <input
+              type="number"
+              className="edit-form-field-input"
+              placeholder="Enter the show's total number of seasons (e.g. 3)"
+              value={media.number_of_seasons}
+              onChange={(e) =>
+                setMedia({ ...media, number_of_seasons: Number(e.target.value) })
+              }
+            />
+         </div>
+        )}
+        <div className="edit-form-field">
+          <div className="form-label">Creator</div>
+          <input
+            className="edit-form-field-input"
+            placeholder="Enter the creator or director name"
+            value={media.creator}
+            onChange={(e) => setMedia({ ...media, creator: e.target.value })}
+          />
+        </div>
+        <div className="edit-form-field">
+          <div className="form-label">Synopsis</div>
+          <textarea
+            className="edit-form-field-textfield"
+            placeholder="Enter a brief description of the movie or show"
+            value={media.synopsis}
+            onChange={(e) => setMedia({ ...media, synopsis: e.target.value })}
+          />
+        </div>
+        <div className="details-actions">
+          <button className="save-btn">
+            Create
+          </button>
+        </div>
+
       </div>
     </div>
   );
