1a35cce06aef5d92cafdead9b620c610383da784
Camila Hernandez <camila.hernandez@ucalgary.ca>
2026-04-06 21:27:38 -0600

    chore: change api import to uppercase Client

--- frontend/src/pages/Watchdata.tsx
+++ frontend/src/pages/Watchdata.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from "react";
-import { api } from "../api/client";
+import { api } from "../api/Client";
 import { FaArrowLeft, FaRegStar, FaStar } from "react-icons/fa";
 
 interface WatchdataProps {
