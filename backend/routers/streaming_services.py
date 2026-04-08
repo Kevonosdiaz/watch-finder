4b0f14b573215b674ed21e199c4b103ce60d0089
Camila Hernandez <camila.hernandez@ucalgary.ca>
2026-04-07 18:38:30 -0600

    feat: add api route to delete a streaming service

--- backend/routers/streaming_services.py
+++ backend/routers/streaming_services.py
@@ -32,3 +32,13 @@ def add_streaming_service(service: StreamingServiceBase, db: Annotated[Session, 
     db.commit()
     db.refresh(new_service)
     return new_service
+
+# Delete a streaming service
+@router.delete("{streaming_service_name}")
+def remove_streaming_service(streaming_service_name: str, db: Annotated[Session, Depends(get_db)]):
+    service = db.query(models.StreamingServices).filter(models.StreamingServices.streaming_service_name == streaming_service_name).first()
+    if not service:
+        raise HTTPException(status_code=404, detail="Streaming service not found")
+    db.delete(service)
+    db.commit()
+    return {"message": "Removed"}
